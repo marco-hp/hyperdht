2da904949cb3dd23fd8eca4aada714c835e3e4f5
marco-hp <marco.dinh.hp@gmail.com>
2026-08-28 20:17:42 +1000

    fix test

--- test/stats.js
+++ test/stats.js
@@ -5,9 +5,7 @@ test('stats', async function (t) {
   const testnet = await swarm(t, 2)
   const dht = testnet.createNode()
 
-  t.alike(dht.stats, {
-    punches: { consistent: 0, random: 0, open: 0, tryLater: 0 },
-    relaying: { attempts: 0, successes: 0, aborts: 0 },
-    socketPool: { socketsAdded: 0, socketsRemoved: 0 }
-  })
+  t.is(dht.stats.punches, { consistent: 0, random: 0, open: 0, tryLater: 0 })
+  t.is(dht.stats.relaying, { attempts: 0, successes: 0, aborts: 0 })
+  t.is(dht.stats.socketPool, { socketsAdded: 0, socketsRemoved: 0 })
 })
